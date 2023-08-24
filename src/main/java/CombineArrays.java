370a01915434779b776ffa7a05d5c8a74fbe9b13
Paul Davis <pdavisDU@gmail.com>
2023-08-24 16:55:50 +0000

    passed testing

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,19 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int a1 = arr1.length;
+        int a2 = arr2.length;
+        int b1 = a1 + a2;
 
-        return null;
+        int [] combined = new int[b1];
+
+        for (int i = 0; i < a1; i++) {
+            combined[i] = arr1[i]; 
+        }
+        for (int i = 0; i < a2; i++) {
+            combined[a1 + i] = arr2[i];
+        }
+
+        return combined;
     }
 }
